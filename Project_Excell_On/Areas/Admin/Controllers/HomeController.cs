6eae2d1ce2a90311b54baf585a211569be6e24ad
phamngocchien2004@@ <114908347+phamngocchien2004@users.noreply.github.com>
2024-05-06 17:36:08 +0700

    layout

---
+++ Project_Excell_On/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_Excell_On.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult History()
+        {
+            return View("History");
+        }
+        public ActionResult Invoice()
+        {
+            return View("Invoice");
+        }
+        public ActionResult CustomerList()
+        {
+            return View("CustomerList");
+        }
+        public ActionResult CustomerDetail()
+        {
+            return View("CustomerDetail");
+        }
+        public ActionResult Department()
+        {
+            return View("Department");
+        }
+    }
+}
---
+++ Project_Excell_On/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_Excell_On.MvcApplication" Language="C#" %>
